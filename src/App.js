68b9cf2fdbd469f6e1a7ee45442ebe6f58b6ad61
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2024-02-28 01:59:53 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { CircularProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
+
+const WINNERS = ['an._xz._', 'xlfiyaaa']
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [countDown, setCountDown] = useState(4);
+
+  const [winner, setWinner] = useState('')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000)
+
+    setWinner(WINNERS[Math.floor(Math.random() * 2)])
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(c => {
+        if(c - 1 > 0) {
+          clearInterval(interval)
+          return c - 1;
+        };
+
+        return c -1;
+      })
+    }, 1000)
+
+    return () => clearInterval(interval);
+
+  }, [countDown])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {
+        !isLoading &&
+        <img style={{ position: 'absolute', zIndex: '2', width: '100%', height: '100%', left: '0' }} src='/Falling Confetti Happy Birthday Gif Pictures, Photos, And Images For  DDE.gif' />
+      }
+      <header style={{ position: 'relative' }} className="App-header">
+        {
+          countDown > 0 && <span style={{ position: 'absolute' }}> { countDown } </span>
+        }
+        {
+          isLoading 
+            ? <CircularProgress size={50} />
+            : <span style={{ position: 'absolute', zIndex: '2', fontWeight: '1000', fontSize: '20vw' }}>
+              { winner }
+            </span>
+        }
+        {
+          isLoading && <img style={{ position: 'absolute', bottom: '5rem' }} src='/party-popper-confetti.gif' />
+        }
       </header>
     </div>
   );
